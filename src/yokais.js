50fd5317242740b772252388925128c9fbdd89a7
joaopedrodcf <joaoferr93@gmail.com>
2018-12-14 00:45:24 +0000

    chore: Add more yokais

--- src/yokais.js
+++ src/yokais.js
@@ -40,6 +40,127 @@ const yokais = [
         evolution: null
     },
     {
+        name: 'Slacka-slash',
+        tribe: 'Brave',
+        rank: 'B',
+        attribute: 'Ice',
+        favoriteFood: 'Juice',
+        evolution: null
+    },
+    {
+        name: 'Mochismo',
+        tribe: 'Brave',
+        rank: 'D',
+        attribute: 'Earth',
+        favoriteFood: 'Rice Balls',
+        evolution: null
+    },
+    {
+        name: 'Minochi',
+        tribe: 'Brave',
+        rank: 'D',
+        attribute: 'Fire',
+        favoriteFood: 'Rice Balls',
+        evolution: null
+    },
+    {
+        name: 'Helmsman',
+        tribe: 'Brave',
+        rank: 'D',
+        attribute: 'Thunder',
+        favoriteFood: 'Vegetables',
+        evolution: '+ Armsman ↓ Reuknight'
+    },
+    {
+        name: 'Reuknight',
+        tribe: 'Brave',
+        rank: 'A',
+        attribute: 'Thunder',
+        favoriteFood: 'Vegetables',
+        evolution: null
+    },
+    {
+        name: 'Corptain',
+        tribe: 'Brave',
+        rank: 'S',
+        attribute: 'Absorption',
+        favoriteFood: 'Vegetables',
+        evolution: null
+    },
+    {
+        name: 'Blazion',
+        tribe: 'Brave',
+        rank: 'D',
+        attribute: 'Fire',
+        favoriteFood: 'Meat',
+        evolution: null
+    },
+    {
+        name: 'Quaken',
+        tribe: 'Brave',
+        rank: 'B',
+        attribute: 'Earth',
+        favoriteFood: 'Meat',
+        evolution: null
+    },
+    {
+        name: 'Siro',
+        tribe: 'Brave',
+        rank: 'A',
+        attribute: 'Thunder',
+        favoriteFood: 'Meat',
+        evolution: null
+    },
+    {
+        name: 'Chansin',
+        tribe: 'Brave',
+        rank: 'C',
+        attribute: 'Fire',
+        favoriteFood: 'Seafood',
+        evolution:
+            '+ Legendary Blade ↓ Sheen + Cursed Blade ↓ Snee + Holy Blade ↓ Gleam'
+    },
+    {
+        name: 'Sheen',
+        tribe: 'Brave',
+        rank: 'A',
+        attribute: 'Wind',
+        favoriteFood: 'Seafood',
+        evolution: null
+    },
+    {
+        name: 'Snee',
+        tribe: 'Brave',
+        rank: 'A',
+        attribute: 'Absorption',
+        favoriteFood: 'Seafood',
+        evolution: null
+    },
+    {
+        name: 'Gleam',
+        tribe: 'Brave',
+        rank: 'S',
+        attribute: 'Thunder',
+        favoriteFood: 'Seafood',
+        evolution: null
+    },
+    {
+        name: 'Benkei',
+        tribe: 'Brave',
+        rank: 'B',
+        attribute: 'Ice',
+        favoriteFood: 'Chinese Food',
+        evolution: null
+    },
+    {
+        name: 'B3-NK1',
+        tribe: 'Brave',
+        rank: 'B',
+        attribute: 'Thunder',
+        favoriteFood: 'Chinese Food',
+        evolution: null
+    },
+    {
         name: 'Darumacho',
         tribe: 'Tough',
         rank: 'B',
