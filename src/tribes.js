30261558e5070016eb1e67bf0114549690767f31
joaopedrodcf <joaoferr93@gmail.com>
2018-12-20 21:41:51 +0000

    feat: Add object tribe name and image

---
+++ src/tribes.js
@@ -0,0 +1,49 @@
+const tribes = [
+    {
+        name: 'Brave',
+        image:
+            'https://res.cloudinary.com/dcrcweea8/image/upload/v1545341843/Yokai/tribes/brave.png'
+    },
+    {
+        name: 'Charming',
+        image:
+            'https://res.cloudinary.com/dcrcweea8/image/upload/v1545341843/Yokai/tribes/charming.png'
+    },
+    {
+        name: 'Eerie',
+        image:
+            'https://res.cloudinary.com/dcrcweea8/image/upload/v1545341843/Yokai/tribes/eerie.png'
+    },
+    {
+        name: 'Heartful',
+        image:
+            'https://res.cloudinary.com/dcrcweea8/image/upload/v1545341843/Yokai/tribes/heartful.png'
+    },
+    {
+        name: 'Mysterious',
+        image:
+            'https://res.cloudinary.com/dcrcweea8/image/upload/v1545341844/Yokai/tribes/mysterious.png'
+    },
+    {
+        name: 'Shady',
+        image:
+            'https://res.cloudinary.com/dcrcweea8/image/upload/v1545341844/Yokai/tribes/shady.png'
+    },
+    {
+        name: 'Slippery',
+        image:
+            'https://res.cloudinary.com/dcrcweea8/image/upload/v1545341845/Yokai/tribes/slippery.png'
+    },
+    {
+        name: 'Tough',
+        image:
+            'https://res.cloudinary.com/dcrcweea8/image/upload/v1545341843/Yokai/tribes/tough.png'
+    },
+    {
+        name: 'Wicked',
+        image:
+            'https://res.cloudinary.com/dcrcweea8/image/upload/v1545341843/Yokai/tribes/wicked.png'
+    }
+];
+
+export default tribes;
