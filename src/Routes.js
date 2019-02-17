7c3a434fdbaaf2f1257a69dbffb7c0fac6368b28
joaopedrodcf <joaoferr93@gmail.com>
2019-02-17 19:04:49 +0000

    feat: Add google analytics tracking per page

--- src/Routes.js
+++ src/Routes.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import createHistory from 'history/createBrowserHistory';
+import ReactGA from 'react-ga';
 import Card from './components/card';
 import Main from './components/main';
 import VersionSelect from './components/version-select';
 import yokaisGame1 from './mocks/yokai-watch-1/yokais';
 import yokaisGame2 from './mocks/yokai-watch-2/yokais';
 import yokaisGame3 from './mocks/yokai-watch-3/yokais';
+
+const history = createHistory();
+history.listen(location => {
+    const page = location.hash.replace('#', '');
+
+    ReactGA.set({ page });
+    ReactGA.pageview(page);
+});
 
 const getYokai = (name, version) => {
     let yokais;
@@ -32,28 +42,41 @@ const getYokai = (name, version) => {
     return yokais.find(yokai => yokai.name === name);
 };
 
-const Routes = ({ gameVersion, changeGameVersion }) => (
-    <Switch>
-        <Route
-            exact
-            path="/home"
-            render={() => <Main gameVersion={gameVersion} />}
-        />
-        <Route
-            exact
-            path="/"
-            render={() => (
-                <VersionSelect changeGameVersion={changeGameVersion} />
-            )}
-        />
-        <Route
-            exact
-            path="/yokai/:name"
-            render={props => (
-                <Card {...getYokai(props.match.params.name, gameVersion)} />
-            )}
-        />
-    </Switch>
-);
+class Routes extends Component {
+    componentDidMount() {
+        const page = window.location.hash.replace('#', '');
+
+        ReactGA.pageview(page);
+    }
+
+    render() {
+        const { gameVersion, changeGameVersion } = this.props;
+        return (
+            <Switch>
+                <Route
+                    exact
+                    path="/home"
+                    render={() => <Main gameVersion={gameVersion} />}
+                />
+                <Route
+                    exact
+                    path="/"
+                    render={() => (
+                        <VersionSelect changeGameVersion={changeGameVersion} />
+                    )}
+                />
+                <Route
+                    exact
+                    path="/yokai/:name"
+                    render={props => (
+                        <Card
+                            {...getYokai(props.match.params.name, gameVersion)}
+                        />
+                    )}
+                />
+            </Switch>
+        );
+    }
+}
 
 export default Routes;
