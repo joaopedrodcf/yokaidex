c86cea1440b9aa52e41f8e64a878f4846e23a32e
joaopedrodcf <joaoferr93@gmail.com>
2020-06-13 18:35:29 +0100

    feat: Add amazon ads

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,58 +1,18 @@
 import React, { useContext } from 'react';
-import { useHistory } from 'react-router';
 import Menu from 'react-feather/dist/icons/menu';
-import ArrowLeft from 'react-feather/dist/icons/arrow-left';
 import { Wrapper } from './style';
-import { GameVersionContext, SidebarContext } from '../../store';
-import utils from '../utils';
+import { SidebarContext } from '../../store';
 
-// TODO: Can't be memo cause of useHistory
 const Header = () => {
     const { handleSidebarOpen } = useContext(SidebarContext);
-    const { gameVersion } = useContext(GameVersionContext);
-    const history = useHistory();
-
-    function goBack() {
-        if (history.length > 2) {
-            history.goBack();
-        } else {
-            history.push(`/yokai-watch-${gameVersion}`);
-        }
-    }
-
-    const matchDeepLocations = () => {
-        return history.location.pathname.split('/').length === 4;
-    };
-
-    const getHeaderText = () => {
-        if (matchDeepLocations()) {
-            return utils.capitalize(
-                history.location.pathname
-                    .split('/')[3]
-                    .replace(/_/g, ' ')
-                    .replace(/boss/g, '')
-            );
-        }
-
-        if (history.location.pathname.split('/').length === 3)
-            return utils.capitalize(history.location.pathname.split('/')[2]);
-
-        return 'Yokaidex';
-    };
 
     return (
         <Wrapper>
-            {matchDeepLocations() ? (
-                <div role="presentation" onClick={goBack}>
-                    <ArrowLeft />
-                </div>
-            ) : (
-                <div role="presentation" onClick={handleSidebarOpen}>
-                    <Menu />
-                </div>
-            )}
+            <div role="presentation" onClick={handleSidebarOpen}>
+                <Menu />
+            </div>
             <div>
-                <h1>{getHeaderText(gameVersion)}</h1>
+                <h1>Yokaidex</h1>
             </div>
         </Wrapper>
     );
